6ba6dafd11676eb46e2bf68ff13e05f7df5682c6
Andrew Meyer <andrewm.bpi@gmail.com>
2015-09-16 11:12:17 -0500

    Add tests

---
+++ test/index.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var Metalsmith = require('metalsmith');
+var frontmatter = require('../lib');
+
+describe('metalsmith-frontmatter', function(){
+  it('should add add metadata based on the frontmatter in the file', function(done){
+    Metalsmith('test/fixtures/basic')
+      .frontmatter(false)
+      .use(frontmatter())
+      .build(function(err, files){
+        if (err) return done(err);
+        assert.equal(files["test.md"].someKey, "value");
+        done();
+      });
+  });
+
+  it('should remove frontmatter from the file contents', function(done){
+    Metalsmith('test/fixtures/basic')
+      .use(frontmatter())
+      .build(function(err, files){
+        if (err) return done(err);
+        assert.equal(files["test.md"].contents.toString(), "# Header\n\nContent\n");
+        done();
+      });
+  });
+});
